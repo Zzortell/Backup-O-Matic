073c3295acb2a294644e334d7a8f39910dc45156
Rémi Blaise <remi.blaise@gmx.fr>
2015-06-13 17:31:52 +0200

    [Tests] Add tests for rights related handling

--- src/Tests/BackupOMaticTest.php
+++ src/Tests/BackupOMaticTest.php
@@ -9,20 +9,20 @@ use Zz\BackupOMatic\Utils\FilesAndFolders;
 class BackupOMaticTest extends \PHPUnit_Framework_TestCase
 {
     protected $testDir = 'tests';
-    protected function moveToTestDir ()
+    
+    public function setUp ()
     {
-    	if ( !file_exists($this->testDir) ) {
-        	mkdir($this->testDir);
+        if ( !file_exists($this->testDir) ) {
+            mkdir($this->testDir);
         }
         chdir($this->testDir);
     }
-    protected function moveToRootDir ()
+    
+    public function tearDown ()
     {
-    	chdir('..');
-    }
-    protected function cleanTestDir ()
-    {
-    	FilesAndFolders::delete($this->testDir);
+        chmod('.', 0777);
+        chdir('..');
+        FilesAndFolders::delete($this->testDir);
     }
     
     /**
@@ -30,8 +30,6 @@ class BackupOMaticTest extends \PHPUnit_Framework_TestCase
      */
     public function test ( $yml )
     {
-        $this->moveToTestDir();
-        
         $config = new YamlConfig ($yml);
         foreach ( $config->getFiles() as $file ) {
             $dir = dirname($file->getPath());
@@ -47,9 +45,68 @@ class BackupOMaticTest extends \PHPUnit_Framework_TestCase
         foreach ( $config->getFiles() as $file ) {
         	$this->assertTrue(file_exists($config->getDir() . DIRECTORY_SEPARATOR . $file->getPath()));
         }
+    }
+    
+    /**
+     * @expectedException               Zz\BackupOMatic\Exception\FailureCopyingException
+     * @expectedExceptionMessageRegExp  #The file ".+" is not readable#
+     */
+    public function testFileNotReadableHandling ()
+    {
+        $config = new YamlConfig (<<<'YML'
+Files:
+    - file
+Backup Directory: backup
+YML
+        );
+        $backupOMatic = new BackupOMatic;
         
-        $this->moveToRootDir();
-        $this->cleanTestDir();
+        touch('file');
+        chmod('file', 0222);
+        
+        $backupOMatic->backup($config);
+    }
+    
+    /**
+     * @expectedException               Zz\BackupOMatic\Exception\FailureCopyingException
+     * @expectedExceptionMessageRegExp  #The folder "." is not writable#
+     */
+    public function testFolderNotWritableHandling ()
+    {
+        $config = new YamlConfig (<<<'YML'
+Files:
+    - file
+Backup Directory: backup
+YML
+        );
+        $backupOMatic = new BackupOMatic;
+        
+        touch('file');
+        chmod('.', 0555);
+        
+        $backupOMatic->backup($config);
+    }
+    
+    /**
+     * @expectedException               Zz\BackupOMatic\Exception\FailureCopyingException
+     * @expectedExceptionMessageRegExp  #The file ".+" is not writable#
+     */
+    public function testFileNotWritableHandling ()
+    {
+        $config = new YamlConfig (<<<'YML'
+Files:
+    - file
+Backup Directory: backup
+YML
+        );
+        $backupOMatic = new BackupOMatic;
+        
+        touch('file');
+        mkdir('backup');
+        touch('backup/file');
+        chmod('backup/file', 0555);
+        
+        $backupOMatic->backup($config);
     }
     
     public function ymlProvider ()
@@ -67,30 +124,19 @@ YML
     }
     
     /**
-     * @dataProvider longYmlProvider
      * @group long
      */
-    public function testHeavy ( $yml ) {
-    	$this->moveToTestDir();
-    	
+    public function testHeavy () {
+        $yml = <<<'YML'
+Files:
+    - file
+Backup Directory: backup
+YML;
     	$config = new YamlConfig ($yml);
         foreach ( $config->getFiles() as $file ) {
         	file_put_contents($file->getPath(), str_pad('', pow(2, 30), 'X'));
         }
         
-        $this->moveToRootDir();
     	$this->test($yml);
-    }
-    
-    public function longYmlProvider ()
-    {
-    	return [
-    		[<<<'YML'
-Files:
-    - file
-Backup Directory: backup
-YML
-			],
-    	];
     }
 }
